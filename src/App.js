1a5bcd602f50cf44edebca394e1be1311870a38e
Don Yang <adong23664@gmail.com>
2022-03-20 12:02:44 +0800

    upload to github

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import { useEffect, useState } from "react";
+import "./App.css";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function App() {
+  const [avatarURL, setAvatarURL] = useState();
+  const [githubUsername, setGithubUsername] = useState();
+  const [repoData, setRepoData] = useState();
+
+  async function repoDataURL() {
+    //Get repo data about github user
+    await fetch("https://api.github.com/users/adong23664/repos")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(36, result);
+          const list = result.map((item) => (
+            <div className="text-center">
+              <a target="_blank" href={item.svn_url}>
+                {item.name}
+              </a>
+            </div>
+          ));
+          setRepoData(list);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }
+
+  useEffect(() => {
+    fetch("https://api.github.com/users/adong23664")
+      .then((res) => res.json())
+      .then(
+        (result) => {
+          console.log(result);
+          setAvatarURL(result.avatar_url);
+          setGithubUsername(result.login);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App w-100 min-vh-100 justify-content-center align-items-center d-flex flex-column">
+      <Card style={{ width: "18rem" }}>
+        <Card.Img variant="top" src={avatarURL} />
+        <Card.Body>
+          <Card.Title>{githubUsername}</Card.Title>
+
+          <Button variant="primary" onClick={repoDataURL}>
+            List my public repos!
+          </Button>
+        </Card.Body>
+      </Card>
+      {repoData}
     </div>
   );
 }
